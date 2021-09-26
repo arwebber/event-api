dada3be46d5a43474a38ec3fc9b69343fd246685
arwebber <andywebber96@gmail.com>
2021-09-26 15:52:14 -0500

    comment out dotenv to try and build in heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config({ path: '.env' });
+// const dotenv = require('dotenv');
+// dotenv.config({ path: '.env' });
 const PORT = process.env.PORT || '3001';
 const app = express();
 
