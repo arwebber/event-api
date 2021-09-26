de87b08d9465d115bc136e3a9729e71c0b628cdb
arwebber <andywebber96@gmail.com>
2021-09-26 18:10:13 -0500

    add cors headers

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 /**
+ * Add cors headers.
+ */
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+/**
  * Set the routers.
  */
 const eventsRouter = require('./routes/events.js');
