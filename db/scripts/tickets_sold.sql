058d200967dee0e369eaf063c999cbef253afcf6
arwebber <andywebber96@gmail.com>
2021-09-27 16:59:14 -0500

    added tickets sold route

--- db/scripts/tickets_sold.sql
+++ db/scripts/tickets_sold.sql
@@ -3,7 +3,6 @@ CREATE TABLE `s4ofvm3lvcnodbsy`.`TICKETS_SOLD` (
   `event_session_id` INT,
   `quantity` INT NOT NULL,
   `email` VARCHAR(255) NOT NULL,
-  `timestamp` DATETIME,
+  `date_created` DATETIME NOT NULL DEFAULT(NOW()),
   PRIMARY KEY (`tickets_sold_id`),
   FOREIGN KEY (`event_session_id`) REFERENCES EVENT_SESSION(`event_session_id`));
-  
