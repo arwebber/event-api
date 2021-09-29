e94c76b0ed69b4ec4259a531887ecbc49caa018d
arwebber <andywebber96@gmail.com>
2021-09-28 22:06:25 -0500

    added logic for ticket sold endpoint to track ramining

--- db/scripts/tickets_sold.sql
+++ db/scripts/tickets_sold.sql
@@ -2,7 +2,11 @@ CREATE TABLE `s4ofvm3lvcnodbsy`.`TICKETS_SOLD` (
   `tickets_sold_id` INT NOT NULL AUTO_INCREMENT,
   `event_session_id` INT,
   `quantity` INT NOT NULL,
+  `first_name` VARCHAR(255) NOT NULL,
+  `last_name` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
+  `phone` VARCHAR(255) NOT NULL,
+  `company` VARCHAR(255) NOT NULL,
   `date_created` DATETIME NOT NULL DEFAULT(NOW()),
   PRIMARY KEY (`tickets_sold_id`),
   FOREIGN KEY (`event_session_id`) REFERENCES EVENT_SESSION(`event_session_id`));
--- routes/tickets-sold.js
+++ routes/tickets-sold.js
@@ -44,22 +44,33 @@ router.get('/v1/tickets/event/session', async function (req, res) {
 
 /**
  * Add an event session.
+ * 
  */
 router.post('/v1/add/tickets/sold', async function (req, res) {
     try {
         const {
-            event_session_id,
-            quantity,
-            email
+            tickets
         } = req.body;
-        const sqlQuery =
-            'INSERT INTO TICKETS_SOLD (event_session_id, quantity, email) VALUES (?, ?, ?)';
-        const result = await db.query(sqlQuery, [
-            event_session_id,
-            quantity,
-            email
-        ]);
-        res.status(200).json({ tickets_sold_id: result.insertId });
+
+        // console.log('tix', tickets)
+
+        tickets.forEach(async ticket => {
+            const sqlQuery =
+                'INSERT INTO TICKETS_SOLD (event_session_id, quantity, first_name, last_name, email, phone, company) ' +
+                'VALUES (?, ?, ?, ?, ?, ?, ?)';
+            const result = await db.query(sqlQuery, [ticket.cartItem.event_session_id, ticket.cartItem.quantity, ticket.firstName, ticket.lastName, ticket.email, ticket.phone, ticket.company]);
+        });
+
+        tickets.forEach(async ticket => {
+            const sqlQuery =
+                'DELETE FROM CART_ITEM WHERE cart_item_id = ?';
+            const result = await db.query(sqlQuery, ticket.cartItem.cart_item_id);
+        });
+
+        const sqlQuery = 'DELETE FROM CART WHERE cart_id = ?';
+        const result = await db.query(sqlQuery, tickets[0].cartItem.cart_id);
+
+        res.status(200).json();
     } catch (error) {
         res.status(400).send(error.message);
     }
