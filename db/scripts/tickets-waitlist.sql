5cffeb5c3f6acdd38f0ef2de34f717dfe1b146f1
arwebber <andywebber96@gmail.com>
2021-09-29 21:34:41 -0500

    remove old insert statements

--- db/scripts/tickets-waitlist.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `s4ofvm3lvcnodbsy`.`TICKETS_WAITLIST` (
-  `waitlist_id` INT NOT NULL AUTO_INCREMENT,
-  `event_session_id` INT,
-  `quantity` INT NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `date_created` DATETIME NOT NULL DEFAULT(NOW()),
-  PRIMARY KEY (`waitlist_id`),
-  FOREIGN KEY (`event_session_id`) REFERENCES EVENT_SESSION(`event_session_id`));
